e958d4e7cd0abbbc395ac882c7a519f7f563ae99
A169800 <A169800@PCS41678.grupo.cm.es>
2013-08-30 08:51:45 +0200

    Commit inicial 30082013

---
+++ Proyecto001/src/nucleoPrincipal/Arranque.java
@@ -0,0 +1,12 @@
+package nucleoPrincipal;
+
+public class Arranque {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Inicio operaciones");
+
+	}
+
+}
